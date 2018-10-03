4a6c54464cf4d13a62304bd3acc613eedb5e5234
slowbirdoflsh <770833105@qq.com>
2018-10-03 17:52:19 +0800

    first commit

---
+++ src/com/lsh/filesfound/FilesFoundTest.java
@@ -0,0 +1,16 @@
+package com.lsh.filesfound;
+
+/**
+ * 	≤‚ ‘∑Ω∑®
+    * @ClassName: FilesFoundTest
+    * @author lingsh
+    * @date Oct 1, 2018
+    *
+ */
+public class FilesFoundTest {
+
+	public static void main(String[] args) {
+		new View();
+	}
+
+}
